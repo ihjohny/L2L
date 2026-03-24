3829c0362eb254057576e29525c65200264a0128
Imam Hossain <iimamhossain23@gmail.com>
2026-03-24 22:23:49 +0600

    Code Clean Up

---
+++ services/src/modules/links/index.ts
@@ -0,0 +1,3 @@
+export { default as linkService } from './link.service';
+export { default as Link } from './link.controller';
+export { default as linkRoutes } from './link.routes';
---
+++ services/src/modules/project/index.ts
@@ -0,0 +1,3 @@
+export { default as projectService } from './project.service';
+export { default as Project } from './project.controller';
+export { default as projectRoutes } from './project.routes';
