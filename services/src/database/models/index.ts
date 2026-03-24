6bb726c163a95f8d3fc70969c6f1d81691d4827a
Imam Hossain <iimamhossain23@gmail.com>
2026-03-24 22:04:50 +0600

    Code Clean Up

--- services/src/database/models/index.ts
+++ services/src/database/models/index.ts
@@ -1,13 +1,5 @@
-// MVP Database Models
 export { default as User } from './User.model';
 export { default as Link } from './Link.model';
 export { default as Project } from './Project.model';
 export { default as AiOutput } from './AiOutput.model';
 export { default as Job } from './Job.model';
-
-// Re-export model interfaces
-export * from './User.model';
-export * from './AiOutput.model';
-export * from './Project.model';
-export * from './Link.model';
-export * from './Job.model';
