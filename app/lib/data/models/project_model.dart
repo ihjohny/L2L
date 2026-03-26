ac94dcc6c2b8204ee9f39b8b9ddd24b2c15d9e4f
Imam Hossain <iimamhossain23@gmail.com>
2026-03-26 20:15:06 +0600

    Code Run

--- app/lib/data/models/project_model.dart
+++ app/lib/data/models/project_model.dart
@@ -1,7 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'project_model.freezed.dart';
-part 'project_model.g.dart';
 
 @freezed
 class ProjectModel with _$ProjectModel {
